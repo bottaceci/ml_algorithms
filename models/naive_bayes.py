75155af95f1b116e0f687fe8242062bc1de745ee
Cecilia Botta <->
2026-05-18 01:01:54 +0200

    Naive Bayes

---
+++ models/naive_bayes.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class NaiveBayes:
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # calculate the mean, the var, and prior for each class
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[y==c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+    
+    def _predict(self, x):
+        posteriors = []
+
+        # calculate the posterior probability for each class
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = posterior + prior
+            posteriors.append(posterior)
+
+        # return the class with the highest posterior
+        return self._classes[np.argmax(posteriors)]
+    
+    def _pdf(self, idx, x):
+        mean = self._mean[idx]
+        var = self._var[idx]
+        exponential = np.exp(-(x-mean)**2 / (2*var))
+        normalization = 1 / np.sqrt(2*np.pi*var)
+        return normalization * exponential
