4f49f0500edc4bbda0cdfcf9ece8f105429529dd
Cecilia Botta <->
2026-05-31 18:32:54 +0200

    PCA Exploration

--- models/pca.py
+++ models/pca.py
@@ -5,31 +5,44 @@ class PCA:
         self.n_components = n_components
         self.mean = None
         self.components = None
+        self.explained_variance = None
+        self.explained_variance_ratio = None
 
     def fit(self, X): # don't need class labels because this is unsupervised training
+        X = np.asarray(X)
+        
         # mean centering
         self.mean = np.mean(X, axis=0)
-        X = X - self.mean
+        X_centered = X - self.mean
 
         # covariance, cov function needs samples as columns -> transpose
-        cov = np.cov(X.T)
+        cov = np.cov(X_centered.T)
 
         # eigenvectors, eigenvalues
-        eigvecs, eigvals = np.linalg.eig(cov)
-
-        # eigenvectors v = [:,i] originally column vector, transpose this for easier calculations
-        eigvecs = eigvecs.T
+        eigvals, eigvecs = np.linalg.eigh(cov) # used np.linalg.eigh instead of eig because covariance matrix is symmetric
 
         # sort eigenvectors
         idxs = np.argsort(eigvals)[::-1]
         eigvals = eigvals[idxs]
-        eigvecs = eigvecs[idxs]
+        eigvecs = eigvecs[:, idxs]
 
         # store the first n_components eigenvectors 
-        self.components = eigvecs[:self.n_components]
+        self.components = eigvecs[:, :self.n_components].T
+        self.explained_variance = eigvals[:self.n_components]
+        self.explained_variance_ratio = (
+            eigvals[:self.n_components] / np.sum(eigvals)
+        )
 
 
-    def transform(self, X): # this method can receive either the training data or the new testing data
-        # project the data
-        X = X - self.mean
-        return np.dot(X, self.components.T)
+    def transform(self, X):
+        if self.components is None:
+            raise ValueError("PCA has not been fitted yet.")
+
+        X = np.asarray(X)
+        X_centered = X - self.mean
+
+        return np.dot(X_centered, self.components.T)
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
